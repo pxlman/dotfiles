a3d3f1b74749ccdbfd99f7f87f7b2f743113a68e
pxlman <agxgv7573@gmail.com>
2024-12-06 16:39:53 +0200

    [+]

--- system/configuration.nix
+++ system/configuration.nix
@@ -441,7 +441,7 @@
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 60;
+      CPU_MAX_PERF_ON_BAT = 40;
 
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
